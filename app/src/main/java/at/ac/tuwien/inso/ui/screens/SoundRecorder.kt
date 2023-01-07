add2026506dac50fa8b8bfd348e8529f75098282
vladfreeze <vladfreeze@gmail.com>
2023-01-08 00:47:32 +0100

    fix: Added center button for recording. Reverted to storage permissions. Removed unused semicolon

--- app/src/main/java/at/ac/tuwien/inso/ui/screens/SoundRecorder.kt
+++ app/src/main/java/at/ac/tuwien/inso/ui/screens/SoundRecorder.kt
@@ -1,7 +1,6 @@
 package at.ac.tuwien.inso.ui.screens
 
 import android.Manifest
-import android.content.pm.PackageManager
 import android.media.MediaRecorder
 import android.os.Build
 import androidx.annotation.RequiresApi
@@ -13,6 +12,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.Alignment
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import at.ac.tuwien.inso.model.Song
@@ -29,7 +29,6 @@ import org.koin.androidx.compose.getViewModel
 import java.io.File
 import java.io.IOException
 import java.util.*
-import java.util.Base64.getEncoder
 
 
 @OptIn(ExperimentalPermissionsApi::class)
@@ -40,90 +39,96 @@ fun SoundRecorder(navController: NavController, viewModel: GenerateCoverViewMode
     var isFinished by remember { mutableStateOf(false) }
     val coroutineScope = rememberCoroutineScope()
     val context = LocalContext.current
-    val allPermissionsState1 = rememberMultiplePermissionsState(
+    val allPermissionsState = rememberMultiplePermissionsState(
         listOf(
-            Manifest.permission.MANAGE_EXTERNAL_STORAGE,
-            Manifest.permission.RECORD_AUDIO
-        )
-    )
-    val allPermissionsState2 = rememberMultiplePermissionsState(
-        listOf(
+            Manifest.permission.RECORD_AUDIO,
             Manifest.permission.WRITE_EXTERNAL_STORAGE,
-            Manifest.permission.READ_EXTERNAL_STORAGE,
-            Manifest.permission.RECORD_AUDIO
-        )
+            Manifest.permission.READ_EXTERNAL_STORAGE
+            )
     )
     LaunchedEffect(isFinished) {
         if(isFinished) {
             navController.navigate(route = SoundViewScreens.ImageGenerateDevToolScreen.route)
         }
     }
-    if (allPermissionsState1.allPermissionsGranted || allPermissionsState2.allPermissionsGranted)   {
+    if (allPermissionsState.allPermissionsGranted) {
         Text("Permission Granted")
-        Button(
-            onClick = {
-                val file = File(context.filesDir.path, "/tmpaudio/")
-                MediaRecorder(context).apply {
-                    setAudioSource(MediaRecorder.AudioSource.MIC)
-                    setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight()
+                .padding(20.dp)) {
+            Button(
+                onClick = {
+                    val file = File(context.filesDir.path, "/tmpaudio/")
+                    MediaRecorder(context).apply {
+                        setAudioSource(MediaRecorder.AudioSource.MIC)
+                        setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
 
-                    if (!file.exists()){
-                        file.mkdirs();
-                    }
-                    val fileName = file.toString() + "/" + Date() + ".ogg"
-                    setOutputFile(fileName)
-                    println(fileName)
-                    setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
-
-                    try {
-                        prepare()
-                    } catch (_: IOException) {
-
-                    }
-
-                    start()
-                    isRecording = true
-                    coroutineScope.launch {
-                        withContext(Dispatchers.IO) {
-                            delay(10000) // Record for 10 seconds
-                            stop()
-                            release()
-                            val fileRead = File(fileName)
-                            val binaryData = fileRead.readBytes()
-                            if (!Python.isStarted()) {
-                                Python.start(AndroidPlatform(context))
-                            }
-                            val py = Python.getInstance()
-                            val module = py.getModule("image_generate")
-                            try {
-                                val text = module.callAttr("scan_song", binaryData)
-                                    .toString()
-                                val jsonObj = JSONObject(text).getJSONObject("result")
-                                val song = Song(
-                                    id = 0,
-                                    artist = jsonObj.get("artist").toString(),
-                                    title = jsonObj.get("title").toString()
-                                )
-                                viewModel.setSong(song)
-                                println(song)
-                                isFinished = true
-                                // From python script we get a PyObject, which is converted to a string. Afterwards
-                                // its added to urlList, so that we can select the urls through indexing
-                            } catch (e: PyException) {
-                                println(e.message + " ")
-                            }
-                            isRecording = false
-                            println(isFinished)
+                        if (!file.exists()) {
+                            file.mkdirs()
                         }
+                        val fileName = file.toString() + "/" + Date() + ".ogg"
+                        setOutputFile(fileName)
+                        println(fileName)
+                        setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
+
+                        try {
+                            prepare()
+                        } catch (_: IOException) {
+
+                        }
+
+                        start()
+                        isRecording = true
+                        coroutineScope.launch {
+                            withContext(Dispatchers.IO) {
+                                delay(10000) // Record for 10 seconds
+                                stop()
+                                release()
+                                val fileRead = File(fileName)
+                                println("!!!!!!!!!!!!!!!!!!!!")
+                                println(fileRead)
+                                val binaryData = fileRead.readBytes()
+                                println(binaryData)
+                                if (!Python.isStarted()) {
+                                    Python.start(AndroidPlatform(context))
+                                }
+                                val py = Python.getInstance()
+                                val module = py.getModule("image_generate")
+                                try {
+                                    val text = module.callAttr("scan_song", binaryData)
+                                        .toString()
+                                    println(text)
+                                    val jsonObj = JSONObject(text).getJSONObject("result")
+                                    val song = Song(
+                                        id = 0,
+                                        artist = jsonObj.get("artist").toString(),
+                                        title = jsonObj.get("title").toString()
+                                    )
+                                    viewModel.setSong(song)
+                                    println(song)
+                                    isFinished = true
+                                    // From python script we get a PyObject, which is converted to a string. Afterwards
+                                    // its added to urlList, so that we can select the urls through indexing
+                                } catch (e: PyException) {
+                                    println(e.message + " ")
+                                }
+                                isRecording = false
+                                println(isFinished)
+                            }
+                        }
+
+
                     }
 
-
-                }
-
-            },
-            enabled = !isRecording
-        ) {
-            Text("Record Audio")
+                },
+                enabled = !isRecording
+            ) {
+                Text("Record Audio")
+            }
         }
         if (isRecording) {
             Box(modifier = Modifier.padding(8.dp)) {
@@ -133,17 +138,15 @@ fun SoundRecorder(navController: NavController, viewModel: GenerateCoverViewMode
     }
     else {
         Column {
-            val allPermissionsRevoked1 =
-                allPermissionsState1.permissions.size ==
-                        allPermissionsState1.revokedPermissions.size
-            val allPermissionsRevoked2 =
-                allPermissionsState2.permissions.size ==
-                        allPermissionsState2.revokedPermissions.size
-            val textToShow = if (!allPermissionsRevoked1 || !allPermissionsRevoked2) {
+            val allPermissionsRevoked =
+                allPermissionsState.permissions.size ==
+                        allPermissionsState.revokedPermissions.size
+
+            val textToShow = if (!allPermissionsRevoked) {
                 // If not all the permissions are revoked, it's because the user accepted the COARSE
                 // location permission, but not the FINE one.
                 "Yay! Thanks for letting me access your Audio and File System. "
-            } else if (allPermissionsState1.shouldShowRationale || allPermissionsState2.shouldShowRationale) {
+            } else if (allPermissionsState.shouldShowRationale) {
                 // Both location permissions have been denied
                 "Getting your Audio while you record is important for this app. " +
                         "Please grant us the Audio Record Permissions. Thank you :D"
@@ -152,7 +155,7 @@ fun SoundRecorder(navController: NavController, viewModel: GenerateCoverViewMode
                 "This feature requires Audio and File permission"
             }
 
-            val buttonText = if (!allPermissionsRevoked1 || !allPermissionsRevoked2) {
+            val buttonText = if (!allPermissionsRevoked) {
                 "Allow Permissions"
             } else {
                 "Request permissions"
@@ -160,10 +163,7 @@ fun SoundRecorder(navController: NavController, viewModel: GenerateCoverViewMode
 
             Text(text = textToShow)
             Spacer(modifier = Modifier.height(8.dp))
-            Button(onClick = { allPermissionsState1.launchMultiplePermissionRequest()
-                allPermissionsState2.launchMultiplePermissionRequest()
-
-            }) {
+            Button(onClick = { allPermissionsState.launchMultiplePermissionRequest() }) {
                 Text(buttonText)
             }
         }
